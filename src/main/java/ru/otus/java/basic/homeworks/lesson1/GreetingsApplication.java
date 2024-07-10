9e3a1f6ff64e9d5093a9d4241be00c89f783584a
{Alexey_Mayorov} <{alexey.mayorov@mvslabs.ru}>
2024-07-10 15:35:11 +0300

    init

---
+++ src/main/java/ru/otus/java/basic/homeworks/lesson1/GreetingsApplication.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homeworks.lesson1;
+
+public class GreetingsApplication {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
